658e24f8e6302166c10e8612df74d40b56b3ea80
kavyashree007 <kavyadevops005@gmail.com>
2025-05-20 17:44:10 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ region = "ap-south-1"
 resource "aws_instance" "AWSEC2Instance"{
  ami = "ami-0af9569868786b23a"
  instance_type = "t2.micro"
- key_name = "devopsmithuntechnologies2025"
+ key_name = "saikavya006"
  security_groups = ["launch-wizard-2"]
  tags = {
  Name = "tomcatservers"
